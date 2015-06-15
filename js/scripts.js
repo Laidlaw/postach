4cbc34a147d4d6d76d9e0c9b2d669ff380dfea91
Alan Laidlaw <alanlaidlaw@gmail.com>
2015-06-15 10:28:30 -0500

    removed jquery, waypoints, et all for perf test

--- js/scripts.js
+++ js/scripts.js
@@ -43,21 +43,21 @@ jQuery(document).ready(function($){
   //   element: $('.header')[0]
   // });
 
-  function closeMenu() {
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
+  // function closeMenu() {
+  //   appbarElement.classList.remove('open');
+  //   navdrawerContainer.classList.remove('open');
+  // }
+  //
+  // function toggleMenu() {
+  //   appbarElement.classList.toggle('open');
+  //   navdrawerContainer.classList.toggle('open');
+  // }
+  //
+  // main.addEventListener('click', closeMenu);
+  // menuBtn.addEventListener('click', toggleMenu);
+  // navdrawerContainer.addEventListener('click', function (event) {
+  //   if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+  //     closeMenu();
+  //   }
+  // });
 })();
